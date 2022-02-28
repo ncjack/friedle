dd484bdd7670a06e50e40021aafc9aa3ae1e40e7
Jackson Taylor <ncjacktaylor@gmail.com>
2022-02-28 16:56:01 -0500

    Freidle Version 1.0

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -8,53 +8,58 @@ type Props = {
 
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title="How to play Friedle" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        This list of names consist of 5 letter names from the following families: 
+        <b> Friedmann, Buckley, Marquardt, Taylor, Roberts, Givens, Reyes, Kroll-Smith</b>
       </p>
+
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        Guess the name in 4 tries. After each guess, the color of the tiles will
+        change to show how close your guess was to the name.
+      </p>     
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="W"
+          value="J"
           status="correct"
         />
-        <Cell value="E" />
         <Cell value="A" />
-        <Cell value="R" />
+        <Cell value="M" />
+        <Cell value="E" />
+        <Cell value="S" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The letter J is in the name and in the correct spot.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="D" />
+        <Cell value="A" />
+        <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="I"
+          status="present"
+        />
+        <Cell value="S" />
         <Cell value="Y" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        The letter I is in the name but in the wrong spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
+        <Cell value="M" />
+        <Cell isRevealing={true} isCompleted={true} value="A" status="absent" />
+        <Cell value="R" />
         <Cell value="I" />
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="L"
-          status="present"
-        />
         <Cell value="O" />
-        <Cell value="T" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
-        <Cell value="A" />
-        <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
-        <Cell value="E" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        The letter A is not in the name in any spot.
       </p>
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
